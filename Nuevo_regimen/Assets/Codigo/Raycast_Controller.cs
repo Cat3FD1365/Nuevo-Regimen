68075443a54fa754f8b7567a9a15cf41a741ffc2
CaroBuenrostro19 <70624310+CaroBuenrostro19@users.noreply.github.com>
2022-03-11 13:35:25 -0600

    Instalación de cámaras
    Las cámaras detectan al personaje y ya están instaladas en la escena.

--- Nuevo_regimen/Assets/Codigo/Raycast_Controller.cs
+++ Nuevo_regimen/Assets/Codigo/Raycast_Controller.cs
@@ -8,13 +8,13 @@ public class Raycast_Controller : MonoBehaviour
     [SerializeField] LayerMask layermask;
     void Update()
     {
-        if(Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out RaycastHit hitinfo, 2f, layermask))
+        if(Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out RaycastHit hitinfo, 10f, layermask))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2f, Color.green);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10f, Color.green);
         }
     }
 }
