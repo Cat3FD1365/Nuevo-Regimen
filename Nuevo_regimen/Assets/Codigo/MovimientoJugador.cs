e4546d8dbdc0a6932244e368233c594ef23317c2
SauloTrujillo <SauloSpartan2@gmail.com>
2022-03-17 19:15:32 -0600

    Movimiento Jugador
    El jugador se mueve mientras la camara se mueve igual, ya funciona correctamente.
    
    !Continua el error de la camara!

--- Nuevo_regimen/Assets/Codigo/MovimientoJugador.cs
+++ Nuevo_regimen/Assets/Codigo/MovimientoJugador.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
+    [SerializeField] CharacterController characterControler;
+
+    float movementSpeed = 12f;
 
     void Start()
     {
@@ -12,6 +15,27 @@ public class MovimientoJugador : MonoBehaviour
 
     void Update()
     {
+        PlayerMovement();
+        Gravity();
+    }
 
+    private void PlayerMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * moveX + transform.forward * moveZ;
+
+        characterControler.Move(moveDirection * movementSpeed * Time.deltaTime);
+    }
+
+    private void Gravity()
+    {
+       Vector3 moveVector = Vector3.zero;
+
+        if (characterControler.isGrounded == false)
+            moveVector += Physics.gravity;
+
+        characterControler.Move(moveVector * Time.deltaTime);
     }
 }
