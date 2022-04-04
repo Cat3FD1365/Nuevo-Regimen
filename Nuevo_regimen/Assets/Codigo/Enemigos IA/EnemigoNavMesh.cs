ba50f36c53624948a3b6ba85fb607fa0bb303899
SauloTrujillo <SauloSpartan2@gmail.com>
2022-04-03 23:32:26 -0500

    Enemigo - Animado
    Ahora los enemigos caminan y corren a la hora de patrullar

--- Nuevo_regimen/Assets/Codigo/Enemigos IA/EnemigoNavMesh.cs
+++ Nuevo_regimen/Assets/Codigo/Enemigos IA/EnemigoNavMesh.cs
@@ -11,8 +11,13 @@ public class EnemigoNavMesh : MonoBehaviour
     int patrolIndex;
     Vector3 targetDestination;
 
+    private Animator anim;
+    private float velocity = 0.0f;
+    [SerializeField] private float acceleration;
+
     void Start()
     {
+        anim = GetComponent<Animator>();
         navEnemy = GetComponent<NavMeshAgent>();
         PatrolDestination();
     }
@@ -24,6 +29,7 @@ public class EnemigoNavMesh : MonoBehaviour
             PatrolPointIteration();
             PatrolDestination();
         }
+        EnemyAnimaton();
     }
 
     private void PatrolDestination()
@@ -37,5 +43,27 @@ public class EnemigoNavMesh : MonoBehaviour
         patrolIndex++;
         if (patrolIndex == patrolPoints.Length)
             patrolIndex = 0;
+    }
+
+    private void EnemyAnimaton()
+    {
+        if (Vector3.Distance(transform.position, targetDestination) < 10 && velocity > 0.0f)
+        {
+            velocity -= Time.deltaTime * acceleration;
+            anim.SetFloat("Speed", velocity);
+        }
+        else if (Vector3.Distance(transform.position, targetDestination) >= 10 && velocity < 1.0f)
+        {
+            velocity += Time.deltaTime * acceleration;
+            anim.SetFloat("Speed", velocity);
+        }
+        else if (velocity < 0.0f)
+        {
+            velocity = 0.0f;
+        }
+        else if (velocity > 1.0f)
+        {
+            velocity = 1.0f;
+        }
     }
 }
