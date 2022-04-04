dc8f8e4cf56cf58e628b39fb0396f9c9d72705f8
SauloTrujillo <SauloSpartan2@gmail.com>
2022-04-03 23:40:26 -0500

    Enemigo - Pruebas Patrullaje Animado
    Otras pruebas de patrullaje animado

--- Nuevo_regimen/Assets/Codigo/Enemigos IA/EnemigoPatrullaje.cs
+++ Nuevo_regimen/Assets/Codigo/Enemigos IA/EnemigoPatrullaje.cs
@@ -11,8 +11,13 @@ public class EnemigoPatrullaje : MonoBehaviour
     [SerializeField] Transform[] availableSpots;
     private int randomSpot;
 
+    private Animator anim;
+    private float velocity = 0.0f;
+    [SerializeField] private float acceleration;
+
     void Start()
     {
+        anim = GetComponent<Animator>();
         waitTime = startWaitTime;
         randomSpot = Random.Range(0, availableSpots.Length);
     }
@@ -20,11 +25,15 @@ public class EnemigoPatrullaje : MonoBehaviour
     void Update()
     {
         EnemyMovement();
+        EnemyAnimaton();
     }
 
     private void EnemyMovement()
     {
-        transform.position = Vector3.MoveTowards(transform.position, availableSpots[randomSpot].position, movementSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, availableSpots[randomSpot].position, 
+            movementSpeed * Time.deltaTime);
+
+        transform.LookAt(availableSpots[randomSpot]);
 
         if (Vector3.Distance(transform.position, availableSpots[randomSpot].position) < 0.2f)
         {
@@ -37,6 +46,28 @@ public class EnemigoPatrullaje : MonoBehaviour
             {
                 waitTime -= Time.deltaTime;
             }
+        }
+    }
+
+    private void EnemyAnimaton()
+    {
+        if (Vector3.Distance(transform.position, availableSpots[randomSpot].position) < 10 && velocity > 0.0f)
+        {
+            velocity -= Time.deltaTime * acceleration;
+            anim.SetFloat("Speed", velocity);
+        }
+        else if (Vector3.Distance(transform.position, availableSpots[randomSpot].position) >= 10 && velocity < 1.0f)
+        {
+            velocity += Time.deltaTime * acceleration;
+            anim.SetFloat("Speed", velocity);
+        }
+        else if (velocity < 0.0f)
+        {
+            velocity = 0.0f;
+        }
+        else if (velocity > 1.0f)
+        {
+            velocity = 1.0f;
         }
     }
 }
