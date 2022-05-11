3d63fbe1f902c1dbac5fa82c630e9bf44732ec5e
elemidiv <angel.emiliano.dv@gmail.com>
2022-05-11 18:41:23 -0500

    Más notificaciones
    +Se agregaron más notificaciones activadas por eventos como estar cerca de un enemigo o una cámara

--- Nuevo_Regimen/Assets/Codigo/Menus e Interfaz/Notificaciones.cs
+++ Nuevo_Regimen/Assets/Codigo/Menus e Interfaz/Notificaciones.cs
@@ -5,42 +5,29 @@ using UnityEngine.UI;
 
 public class Notificaciones : MonoBehaviour
 {
+    //Para logica del texto
     [SerializeField] Text mensaje;
     [SerializeField] string[] consejos;
-    public float opa = 0;
+    private float opa = 0;
     private bool alTope = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        mensajesPredeterminados();
-    }
+    //Para logica de correr
+    private int corrio;
+    private int corr = 1;
+    private int random;
 
-    // Update is called once per frame
-    void Update()
-    {
-        mensaje.color = new Color(mensaje.color.r, mensaje.color.g, mensaje.color.b, opa * 1f);
-        subiendo();
-        bajando();
-    }
+    //Para logica cercania a camaras y enemigos
+    private bool verGuardia;
+    private bool verCamara;
 
 
-    private void mensajesPredeterminados()
-    {
-        //Primero hay una espera luego se pone el mensaje y el aparecer y desaparecer
-
-        //Primer mensaje
-        mensaje.text = consejos[0];
-        StartCoroutine(aparicion());
-
-    }
-
+    //Sobre logica cambio de opacidad
     private void subiendo()
     {
-        if (alTope == false && opa <1)
+        if (alTope == false && opa < 1)
         {
             Debug.Log("Subiendo");
-            opa = opa + 0.05f * Time.deltaTime * 5;
+            opa = opa + 0.06f * Time.deltaTime * 5;
         }
     }
 
@@ -49,28 +36,91 @@ public class Notificaciones : MonoBehaviour
         if (alTope == true && opa > 0)
         {
             Debug.Log("Bajando");
-            opa = opa - 0.05f * Time.deltaTime * 5;
+            opa = opa - 0.07f * Time.deltaTime * 5;
         }
     }
 
-    IEnumerator aparicion()
+    // Start is called before the first frame update
+    void Start()
     {
-        //Empezara a subir
-        yield return new WaitForSeconds(2);
-        alTope = false;
+        StartCoroutine(mensajesPredeterminados());
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //Actualizacion del color
+        mensaje.color = new Color(mensaje.color.r, mensaje.color.g, mensaje.color.b, opa * 1f);
+        subiendo();
+        bajando();
+
+        //Primer vez en correr y aleatoriedad cuando se corre
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            corrio = corr;
+            random = UnityEngine.Random.Range(0, 10);
+            if (corrio == 1 || random == corr)
+            {
+                corr = 2;
+                StartCoroutine(alCorrer());
+            }
+        }
+
+        //Primer encuentro con guardia y camara
+        if (verGuardia == true) StartCoroutine(encuentroGuardia());
+        if (verCamara == true) StartCoroutine(encuentroCamara());
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemigo")) verGuardia = true;
+
+        if (other.CompareTag("Camara")) verCamara = true;
+            
+    }
+
+
+    IEnumerator mensajesPredeterminados()
+    {
+        yield return new WaitForSeconds(5);
+        mensaje.text = consejos[0];
+        alTope = false;
         Debug.Log("Espera");
-        yield return new WaitForSeconds(8);
+        yield return new WaitForSeconds(7);
         Debug.Log("Esperado");
         alTope = true;
-        
+
+        yield return new WaitForSeconds(14);
+        mensaje.text = consejos[1];
+        alTope = false;
+        Debug.Log("Espera");
+        yield return new WaitForSeconds(7);
+        Debug.Log("Esperado");
+        alTope = true;
+
     }
 
-    IEnumerator tiempoEspera(float tiempo)
+    IEnumerator alCorrer()
     {
-        yield return new WaitForSeconds(tiempo);
+        mensaje.text = consejos[2];
+        alTope = false;
+        yield return new WaitForSeconds(7);
+        alTope = true;
     }
 
+    IEnumerator encuentroGuardia()
+    {
+        mensaje.text = consejos[4];
+        alTope = false;
+        yield return new WaitForSeconds(7);
+        alTope = true;
+    }
 
-
+    IEnumerator encuentroCamara()
+    {
+        mensaje.text = consejos[3];
+        alTope = false;
+        yield return new WaitForSeconds(7);
+        alTope = true;
+    }
 }
